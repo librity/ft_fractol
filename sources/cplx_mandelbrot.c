173dac92b2c19fb2c36194cee24e90193b8dd624
Luis Geniole <luis.geniole@gmail.com>
2022-02-20 15:19:36 -0300

    Optimize mandelbrot.

--- sources/cplx_mandelbrot.c
+++ sources/cplx_mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 00:06:42 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/02/20 02:17:06 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/02/20 15:02:18 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,21 +34,21 @@ static t_fractal	slow(t_complex number, int max_iterations,
 static t_fractal	quick(t_complex number, int max_iterations,
 		long double infinity)
 {
-	t_complex	current;
 	int			iteration;
-	long double	temp;
+	t_complex	aux;
+	t_complex	current;
 
-	current = number;
 	iteration = 0;
+	aux = complex(0, 0);
+	current = complex(0, 0);
 	while (iteration < max_iterations)
 	{
-		if (quick_magnitude(current) > infinity)
+		if (current.real + current.imaginary > infinity)
 			return ((t_fractal){true, iteration});
-		temp = current.real;
-		current.real = current.real * current.real;
-		current.real -= current.imaginary * current.imaginary;
-		current.real += number.real;
-		current.imaginary = 2 * current.imaginary * temp + number.imaginary;
+		aux.imaginary = 2 * aux.real * aux.imaginary + number.imaginary;
+		aux.real = current.real - current.imaginary + number.real;
+		current.real = aux.real * aux.real;
+		current.imaginary = aux.imaginary * aux.imaginary;
 		iteration++;
 	}
 	return ((t_fractal){false, iteration});

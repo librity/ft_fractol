3fd913242c5f2ffb72abf384639e347b1ab099b6
Luis Geniole <luis.geniole@gmail.com>
2022-02-22 10:21:50 -0300

    Organize sources in directories.

---
+++ sources/errors.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 00:17:03 by lpaulo-m          #+#    #+#             */
+/*   Updated: 2022/02/22 10:17:52 by lpaulo-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fractol.h>
+
+void	help_and_die(void)
+{
+	ft_putstr(HELP_MSG);
+	exit(EXIT_FAILURE);
+}
+
+void	die(void)
+{
+	perror("ERROR!\n");
+	exit(EXIT_FAILURE);
+}
+
+void	die_if_null(void *ptr)
+{
+	if (ptr == NULL)
+		die();
+}
--- sources/mlx/exit.c
+++ sources/mlx/exit.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 00:17:03 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/02/20 21:06:53 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/02/22 10:17:35 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,4 @@ void	clean_and_exit(t_fractol *ctl)
 	free(ctl->mlx);
 	ft_putendl(EXIT_MSG);
 	exit(EXIT_SUCCESS);
-}
-
-void	help_and_die(void)
-{
-	ft_putstr(HELP_MSG);
-	exit(EXIT_FAILURE);
-}
-
-void	die(void)
-{
-	perror("ERROR!\n");
-	exit(EXIT_FAILURE);
-}
-
-void	die_if_null(void *ptr)
-{
-	if (ptr == NULL)
-		die();
 }

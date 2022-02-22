49bc7fd648e33a1a7695e011b77dd8c473789756
Luis Geniole <luis.geniole@gmail.com>
2022-02-22 13:57:50 -0300

    Fix norm error.

--- sources/complex/julia.c
+++ sources/complex/julia.c
@@ -6,13 +6,15 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 00:06:42 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/02/22 13:29:02 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/02/22 13:57:32 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol.h>
 
-t_fractal	cx_slow_julia(t_complex number, t_complex factor, int max_iterations,
+t_fractal	cx_slow_julia(t_complex number,
+		t_complex factor,
+		int max_iterations,
 		long double infinity)
 {
 	t_complex	current;
